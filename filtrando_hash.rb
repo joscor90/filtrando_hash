13645c14a3dbe1b92bc5acb68cbbb9be6a2fce0a
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-07 09:34:17 -0300

    Added test hash, test variable and test output

--- filtrando_hash.rb
+++ filtrando_hash.rb
@@ -1,9 +1,16 @@
 #filter method
-
 def hash_filter(hash)
     filtered_hash = hash.select {|k, v| v > 70000}
 end
 
 
+#test hash
+ventas = {
+Octubre: 65000,
+Noviembre: 68000,
+Diciembre: 72000
+}
 
-
+#test output
+test = hash_filter(ventas)
+print test
